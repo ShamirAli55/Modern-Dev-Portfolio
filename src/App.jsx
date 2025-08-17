674fd5942e88617eb99899a1c9b3a2a119a9058f
ShamirAli55 <shamirali9779@gmail.com>
2025-08-17 15:19:47 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import HeroSection from './pages/HeroSection'
-import VideoSection from './pages/VideoSection'
-import AboutSection from './pages/AboutSection'
+import Navbar from './sections/Navbar'
+
 const App = () => {
   return (
-    <div className='bg-[#EFEAE3] overflow-x-hidden'>
-      <HeroSection />
-      <VideoSection/>
-      <AboutSection/>
-    </div>
+    <Navbar/>
   )
 }
 
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-scroll";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const NavRef = useRef(null);
+  const MenuT1 = useRef(null);
+  const TopLine = useRef(null);
+  const BottomLine = useRef(null);
+  const iconTl = useRef(null);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      MenuT1.current.play();
+      iconTl.current.play();
+    } else {
+      MenuT1.current.reverse();
+      iconTl.current.reverse();
+    }
+  }, [isOpen]);
+
+  useGSAP(() => {
+    MenuT1.current = gsap
+      .timeline({ paused: true })
+      .from(NavRef.current, {
+        x: 400,
+        opacity: 0,
+        duration: 0.8,
+        delay: 0.01,
+      })
+    iconTl.current = gsap
+      .timeline({ paused: true })
+      .to(TopLine.current, {
+        rotate: 45,
+        y: 3.3,
+        duration: 0.3,
+        ease: "power2.inOut",
+      })
+      .to(
+        BottomLine.current,
+        {
+          rotate: -45,
+          y: -3.3,
+          duration: 0.3,
+          ease: "power2.inOut",
+        },
+        "<"
+      );
+  }, []);
+
+  return (
+    <section className="h-screen w-screen overflow-hidden">
+      <nav
+        className="h-full w-[60%] md:w-1/2 bg-black fixed z-30 right-0 py-16 px-2"
+        ref={NavRef}
+      >
+        <div>
+          {["Home", "About", "Projects", "Contact"].map((links, index) => (
+            <Link
+              className="block relative text-5xl p-3 leading-10 tracking-wide text-left cursor-pointer text-white/60 md:text-7xl md:p-4 md:leading-12 hover:text-white mouse-ovr md:px-6 "
+              key={index}>
+              {links}
+            </Link>
+          ))}
+        </div>
+      </nav>
+      <div
+        className="h-15 w-15 bg-black rounded-full fixed right-3 top-2 flex gap-1 items-center justify-center flex-col md:h-16 md:w-16 md:right-10 cursor-pointer z-40"
+        onClick={() => toggleMenu()} 
+      >
+        <span
+          className="block w-8 h-0.5 bg-white rounded-full origin-center"
+          ref={TopLine}
+        ></span>
+        <span
+          className="block w-8 h-0.5 bg-white rounded-full origin-center"
+          ref={BottomLine}
+        ></span>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
