423f1e4bd2bc02e188ed07c38de4380165d5ff0c
ShamirAli55 <shamirali9779@gmail.com>
2025-08-17 15:34:42 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+import ReactLenis from 'lenis/react'
 
 const App = () => {
   return (
+    <ReactLenis root className='relative'>
+    <div>
     <Navbar/>
+    <Hero/>
+    </div>
+    </ReactLenis>
   )
 }
 
